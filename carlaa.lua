9ba6bfce59a76bf75b99ecb6d66f069b71d1f391
Tenshin <101375880+TenshinRustu@users.noreply.github.com>
2022-11-26 19:37:38 -0700

    Create carlaa.lua

---
+++ carlaa.lua
@@ -0,0 +1,23 @@
+    --//UserSets
+    settings().Physics.PhysicsEnvironmentalThrottle = 1
+    settings().Rendering.QualityLevel = 'Level01'
+    UserSettings():GetService("UserGameSettings").MasterVolume = 0
+for _,v in pairs(game:GetDescendants()) do
+        if v:IsA('Decal') or v:IsA('Texture') or v:IsA('SpotLight') or v:IsA('WedgePart') or v:IsA('UnionOperation') then
+            v:Destroy()
+        elseif v:IsA('Part') then
+            v.Material = Enum.Material.Air
+            v.Transparency = 1
+        elseif v.Name == 'Terrian' then
+            v.WaterReflectace = 1
+            v.WaterTransparency = 1
+        elseif v:IsA('MeshPart') then
+            v.TextureID = 0
+            v.Transparency = 1
+        end
+    end
+for i, v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+     if v:IsA("BasePart") then
+     v:Destroy()
+  end
+end
